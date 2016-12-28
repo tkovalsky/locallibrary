d18d75fb45c6326017821bee318d62c0042228a4
todd <t.e.kovalsky@gmail.com>
2016-12-28 00:08:42 -0500

    moved directories

---
+++ catalog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """
+    View function for the home page of this site
+    """
+    #Generate counts for some main objects
+    num_books=Book.objects.all().count()
+    num_instances=BookInstance.objects.all().count()
+    # Available books (status = 'a')
+    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+    num_authors=Author.objects.count() #'All' is implied by default
+
+    #Render the html template index.html with the data in the context variable
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_books':num_books,
+            'num_instances':num_instances,
+            'num_instances_available':num_instances_available,
+            'num_authors':num_authors
+        },
+    )
